4d99d7f6c49ddfe43d7d97286a8f59e1cc08e37c
Mitchell George <mitchgeorge8@gmail.com>
2022-07-14 23:02:55 -0400

    Added mutations and queries to client side code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,21 +15,12 @@ const typeDefs = gql`
     description: String
     title: String
     image: String
-    linke: String
+    link: String
   }
 
   type Auth {
     token: ID!
     user: User
-  }
-
-  input BookInput {
-    authors: [String]
-    description: String
-    title: String
-    bookId: ID
-    image: String
-    link: String
   }
 
   type Query {
@@ -39,8 +30,15 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: BookInput): User
-    removeBook(bookId: ID): User
+    saveBook(
+      authors: [String]!
+      description: String!
+      title: String!
+      bookId: ID!
+      image: String!
+      link: String!
+    ): User
+    removeBook(bookId: ID!): User
   }
 `;
 
